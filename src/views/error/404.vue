685289688af8272de82f1b04214eed91c6a77f91
766187397 <766187397@qq.com>
2025-05-22 10:58:01 +0800

    "refactor(router): 提取路由守卫逻辑至单独文件并新增403错误页面组件"

---
+++ src/views/error/404.vue
@@ -0,0 +1,9 @@
+<template>
+  <Error></Error>
+</template>
+
+<script setup lang="ts">
+  import Error from "./components/Error.vue";
+</script>
+
+<style scoped></style>
---
+++ src/views/error/components/Error.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="not-found">
+    <div class="content">
+      <h1>{{ errorCode }}</h1>
+      <p>{{ errorMessage }}</p>
+      <el-button type="primary" @click="goHome">返回首页</el-button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+  import { useRouter } from "vue-router";
+
+  const router = useRouter();
+
+  const props = withDefaults(
+    defineProps<{
+      errorCode?: number | string;
+      errorMessage?: string;
+    }>(),
+    {
+      errorCode: 404,
+      errorMessage: "抱歉，您访问的页面不存在",
+    }
+  );
+
+  const goHome = () => {
+    router.push("/");
+  };
+</script>
+
+<style scoped>
+  .not-found {
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #f5f7fa;
+  }
+
+  .content {
+    text-align: center;
+  }
+
+  h1 {
+    font-size: 120px;
+    color: #409eff;
+    margin: 0;
+    line-height: 1.2;
+  }
+
+  p {
+    font-size: 24px;
+    color: #606266;
+    margin: 20px 0 30px;
+  }
+</style>
