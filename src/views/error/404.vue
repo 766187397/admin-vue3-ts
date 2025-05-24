66d3abc6b330029e6ed4e0913d7821efbd5dcef9
胡尚坤 <766187397@qq.com>
2025-05-24 14:53:06 +0800

    refactor(error): 新增404页面路由调试日志输出，引入vue-router相关hooks

--- src/views/error/404.vue
+++ src/views/error/404.vue
@@ -4,6 +4,11 @@
 
 <script setup lang="ts">
   import Error from "./components/Error.vue";
+  import { useRouter, useRoute } from "vue-router";
+  const router = useRouter();
+  const route = useRoute();
+  console.log('router', router)
+  console.log('route', route)
 </script>
 
 <style scoped></style>
