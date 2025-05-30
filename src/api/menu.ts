5a69303efe2cbc9b3d94e682e029157f4beda1e7
766187397 <766187397@qq.com>
2025-05-30 14:39:22 +0800

    feat(route): 优化路由服务排序逻辑，新增AdminRoute.vue表格视图及ElCol/ElRow组件声明

--- src/api/menu.ts
+++ src/api/menu.ts
@@ -2,7 +2,7 @@ import type { Result } from "@/types/api";
 import type { getRoutesParams, RoleRoutes } from "@/types/menu";
 import { http } from "@/utils/http";
 
-/** 获取路由列表 */
+/** 根据用户绑定的角色获取路由列表 */
 export const getRoutes = async (params?: getRoutesParams): Promise<Result<RoleRoutes[]>> => {
   return await http.get<any>("/api/v1/admin/routes/by/role", params);
 };
--- src/views/system/route/AdminRoute.vue
+++ src/views/system/route/AdminRoute.vue
@@ -1,7 +1,29 @@
 <template>
-  <div>路由</div>
+  <div class="admin_route">
+    <el-form :model="query" label-width="auto">
+      <el-row :gutter="20">
+        <el-col :span="4">
+          <el-form-item label="类型：">
+            <el-input v-model="query.type" />
+          </el-form-item>
+        </el-col>
+      </el-row>
+    </el-form>
+
+    <el-table :data="tableData" row-key="id" default-expand-all>
+      <el-table-column prop="date" label="Date" />
+      <el-table-column prop="name" label="Name" />
+      <el-table-column prop="address" label="Address" />
+    </el-table>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  const query = reactive({
+    type: "",
+  });
+
+  const tableData = reactive([]);
+</script>
 
 <style lang="scss" scoped></style>
