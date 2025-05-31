ee34a4dcea98c4982956eb66e03ac4d073eec2e9
胡尚坤 <766187397@qq.com>
2025-05-31 15:57:16 +0800

    feat(public): 重构公共状态存储模块，简化代码结构并优化全屏状态管理逻辑

--- src/store/modules/public.ts
+++ src/store/modules/public.ts
@@ -3,73 +3,67 @@ import { defineStore } from "pinia";
 import { ref } from "vue";
 
 /** 公共 */
-export const usePublicStore = defineStore(
-  "public",
-  () => {
-    // 全屏状态
-    let fullScreen = ref<boolean>();
+export const usePublicStore = defineStore("public", () => {
+  // 全屏状态
+  let fullScreen = ref<boolean>();
 
-    /** 获取并设置全屏状态 */
-    const getFullScreenState = () => {
-      if (document.fullscreenElement) {
+  /** 获取并设置全屏状态 */
+  const getFullScreenState = () => {
+    if (document.fullscreenElement) {
+      fullScreen.value = true;
+    } else {
+      fullScreen.value = false;
+    }
+  };
+
+  /** 进入 */
+  const enterFullScreen = () => {
+    document
+      .querySelector("html")
+      ?.requestFullscreen()
+      .then(() => {
         fullScreen.value = true;
-      } else {
+      })
+      .catch((error) => {
         fullScreen.value = false;
-      }
-    };
+      });
+  };
+  /** 退出 */
+  const exitFullscreen = () => {
+    document
+      .exitFullscreen()
+      .then(() => {
+        fullScreen.value = false;
+      })
+      .catch((error) => {
+        fullScreen.value = true;
+      });
+  };
 
-    /** 进入 */
-    const enterFullScreen = () => {
-      document
-        .querySelector("html")
-        ?.requestFullscreen()
-        .then(() => {
-          fullScreen.value = true;
-        })
-        .catch((error) => {
-          fullScreen.value = false;
-        });
-    };
-    /** 退出 */
-    const exitFullscreen = () => {
-      document
-        .exitFullscreen()
-        .then(() => {
-          fullScreen.value = false;
-        })
-        .catch((error) => {
-          fullScreen.value = true;
-        });
-    };
+  /** 平台类型列表 */
+  const platformList = ref<getDictionaryItemAllResult[]>();
 
-    /** 平台类型列表 */
-    const platformList = ref<getDictionaryItemAllResult[]>();
+  /** 设置平台类型列表 */
+  const setPlatformList = (list: getDictionaryItemAllResult[]) => {
+    platformList.value = list;
+  };
 
-    /** 设置平台类型列表 */
-    const setPlatformList = (list: getDictionaryItemAllResult[]) => {
-      platformList.value = list;
-    };
+  /** 公共状态 */
+  const publicStateList = ref<getDictionaryItemAllResult[]>();
 
-    /** 公共状态 */
-    const publicStateList = ref<getDictionaryItemAllResult[]>();
+  /** 设置公共状态 */
+  const setPublicStateList = (state: getDictionaryItemAllResult[]) => {
+    publicStateList.value = state;
+  };
 
-    /** 设置公共状态 */
-    const setPublicStateList = (state: getDictionaryItemAllResult[]) => {
-      publicStateList.value = state;
-    };
-
-    return {
-      fullScreen,
-      getFullScreenState,
-      enterFullScreen,
-      exitFullscreen,
-      platformList,
-      setPlatformList,
-      publicStateList,
-      setPublicStateList,
-    };
-  },
-  {
-    persist: true,
-  }
-);
+  return {
+    fullScreen,
+    getFullScreenState,
+    enterFullScreen,
+    exitFullscreen,
+    platformList,
+    setPlatformList,
+    publicStateList,
+    setPublicStateList,
+  };
+});
